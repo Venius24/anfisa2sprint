2fea650184299ab3ab3b321c2c44953be24d9506
Venius24 <WonderRebirth@mail.ru>
2025-11-16 16:15:28 +0500

    querysets

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -1,4 +1,6 @@
+import debug_toolbar
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('ice_cream/', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),) 
