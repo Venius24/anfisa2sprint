98ec6f18c64f7d4ea3490e25404300351594bcad
Venius24 <WonderRebirth@mail.ru>
2025-11-23 00:47:35 +0500

    Changes after hospital

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -4,8 +4,16 @@ from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title')
-    # Полученный из БД QuerySet передаём в словарь контекста:
+
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+    ).filter(
+        # Проверяем, что
+        is_published=True,  # Сорт разрешён к публикации;
+        is_on_main=True,  # Сорт разрешён к публикации на главной странице;
+        category__is_published=True  # Категория разрешена к публикации.
+    )
+
     context = {
         'ice_cream_list': ice_cream_list,
     }
